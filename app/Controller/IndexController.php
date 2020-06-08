c81701d57e4a4972f67be81f580bf6886a5e3e2f
sunbinbin <sunbinbin@chelun.com>
2020-06-08 17:39:35 +0800

    测试

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -12,11 +12,33 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Hyperf\HttpServer\Annotation\Controller;
+use Hyperf\HttpServer\Annotation\RequestMapping;
+
+/**
+ * Class IndexController
+ * @package App\Controller
+ * @Controller()
+ */
 class IndexController extends AbstractController
 {
     public function index()
     {
         $user = $this->request->input('user', 'I love NanNan');
+        $method = $this->request->getMethod();
+
+        return [
+            'method' => $method,
+            'message' => "Hello {$user}.",
+        ];
+    }
+
+    /**
+     * @RequestMapping(path="test", methods="get,post")
+     */
+    public function test()
+    {
+        $user = $this->request->input('user', 'I am Test');
         $method = $this->request->getMethod();
 
         return [
