73fc99c027c34e4691147022208f7be2f92f79d9
sunbinbin <sunbinbin@chelun.com>
2020-06-09 14:35:29 +0800

    登录

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -12,7 +12,6 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Request\LoginRequest;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\RequestMapping;
