a352144393919355637ad7a02bfd884a0219b397
sunbinbin <sunbinbin@chelun.com>
2020-06-09 10:56:38 +0800

    数据库配置

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -17,7 +17,7 @@ return [
         'database' => env('DB_DATABASE', 'hyperf'),
         'port' => env('DB_PORT', 3306),
         'username' => env('DB_USERNAME', 'root'),
-        'password' => env('DB_PASSWORD', ''),
+        'password' => env('DB_PASSWORD', 'TYsky5203344!'),
         'charset' => env('DB_CHARSET', 'utf8'),
         'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
         'prefix' => env('DB_PREFIX', ''),
