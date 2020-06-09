a40dc1959967e2af15c407e45f7a86a18f9e9866
sunbinbin <sunbinbin@chelun.com>
2020-06-09 14:13:18 +0800

    登录

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -13,9 +13,12 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Request\LoginRequest;
+use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\RequestMapping;
 use Hyperf\DbConnection\Db;
+use Hyperf\HttpServer\Contract\RequestInterface;
+use Hyperf\Validation\Contract\ValidatorFactoryInterface;
 
 /**
  * Class IndexController
@@ -24,16 +27,49 @@ use Hyperf\DbConnection\Db;
  */
 class UserController extends AbstractController
 {
+
+    /**
+     * @Inject()
+     * @var ValidatorFactoryInterface
+     */
+    protected $validationFactory;
+
     /**
      * @RequestMapping(path="login", methods="get,post")
      */
-    public function login(LoginRequest $request)
+    public function login(RequestInterface $request)
     {
-        $user = $this->request->input('user');
-        $password = $this->request->input('password');
-        $validated = $request->validated();
-        print_r($validated);
-//        Db::table('user')->where('')
+        $validator = $this->validationFactory->make(
+            $request->all(),
+            [
+                'name' => 'required|max:20',
+                'password' => 'required|max:50',
+            ],
+            [
+                'name.required' => 'name is required',
+                'password.required' => 'password is required',
+            ]
+        );
+
+        //未通过验证
+        if ($validator->fails()){
+            // Handle exception
+            $errorMessage = $validator->errors()->first();
+            return [
+                'error' => 500,
+                'message' => $errorMessage,
+            ];
+        }
+
+        //通过验证
+        $name = $request->input('name');
+        $password = $request->input('password');
+        $where = [
+            ['name','=',$name],
+            ['password','=',md5($password)],
+        ];
+        $user = Db::table('user')->where($where)->first();
+        print_r($user);die;
 
 //        return [
 //            'method' => $method,
